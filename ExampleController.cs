ee9c395a8a40d11bb76d61a82f18de78e2a72ff3
DakkaOrk <malickcaputto@yahoo.com>
2020-08-16 00:18:59 -0700

    Add files via upload

---
+++ ExampleController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyBlogger.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult ShowMeAPage()
+        {
+            return View();  
+        }
+
+        public IActionResult ShowMeAnError()
+        {
+            return NotFound();
+        }
+
+        public IActionResult GiveMeAFile()
+        {
+            return File("/mydoc.txt", "text/plain");
+        }
+
+        public IActionResult GiveMeSomeJason()
+        {
+            string jsondata = "{Name: John-luc Picard, Rank: Captain, Starship: Enterprise}";
+
+            return Json(jsondata);
+        }
+
+        public IActionResult TakeMeToGoogle()
+        {
+            return Redirect("https://google.com");
+        }
+    }
+}
